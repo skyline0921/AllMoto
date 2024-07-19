97caaf5253d29d8eccb6051c79e1c76e789191b7
Bryan <bryansrebello@gmail.com>
2024-07-19 15:38:02 -0300

    historico,botões e etc

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageComponent } from './page/page.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  {
+    path: 'page',
+    component: PageComponent
+  },
+  {
+    path:'**',
+    redirectTo: 'page'
+  }
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
